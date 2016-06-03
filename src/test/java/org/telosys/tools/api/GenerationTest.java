1af9f1f5b4fda540f123164a9607c85c5d078ffc
Laurent Guerin <laguerin@LFR002634.corp.capgemini.com>
2016-06-03 17:32:47 +0200

    More tests based on real code generation

---
+++ src/test/java/org/telosys/tools/api/GenerationTest.java
@@ -0,0 +1,95 @@
+package org.telosys.tools.api;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.telosys.tools.commons.cfg.TelosysToolsCfg;
+import org.telosys.tools.generator.task.ErrorReport;
+import org.telosys.tools.generator.task.GenerationTaskResult;
+import org.telosys.tools.generic.model.Model;
+
+public class GenerationTest {
+	
+	private final static String TEST_BUNDLE1 = "test-bundle1";
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Before test");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("After test");
+	}
+
+	private TelosysProject initProject() throws Exception {
+
+		String projectFolderFullPath = TestsEnv.createProjectFolder("myproject");
+		TelosysProject telosysProject = new TelosysProject(projectFolderFullPath);
+		
+		System.out.println("Init project...");
+		String s = telosysProject.initProject();
+		System.out.println(s);
+		
+//		TestsEnv.copyDbModelFile(projectFolderFullPath, "bookstore.dbrep");
+		TestsEnv.copyDslModelFiles(projectFolderFullPath, "employees");
+		TestsEnv.copyDslModelFiles(projectFolderFullPath, "cars-and-drivers");
+//		TestsEnv.copyDslModelFiles(projectFolderFullPath, "employees_invalid");
+//		TestsEnv.copyTemplatesFiles(projectFolderFullPath, "basic-templates-TT210");
+		TestsEnv.copyTemplatesFiles(projectFolderFullPath, TEST_BUNDLE1 );
+		
+		return telosysProject ;
+	}
+	
+	@Test
+	public void testModelDSL_CarsAndDrivers() throws Exception {
+		System.out.println("========== Generation for model 'Cars and Drivers'");
+		System.out.println(" --- Init Project ..." );
+		TelosysProject telosysProject = initProject() ;
+		System.out.println("Project folder : " + telosysProject.getProjectFolder() );
+		System.out.println("getTelosysToolsCfg...");
+		TelosysToolsCfg telosysToolsCfg = telosysProject.getTelosysToolsCfg();
+		
+		System.out.println(" --- Load Model ..." );
+		GenericModelLoader genericModelLoader =  new GenericModelLoader(telosysToolsCfg);
+		Model model = genericModelLoader.loadModel("cars-and-drivers.model");
+		assertNotNull(model);
+		System.out.println("Model loaded. Model name = " + model.getName() );
+
+		String bundleName = TEST_BUNDLE1 ;
+		System.out.println(" --- Launch code generation for bundle '" + bundleName  + "' ..." );
+		GenerationTaskResult result = telosysProject.launchGeneration(model, bundleName);
+		
+		System.out.println("Error(s) count = " + result.getNumberOfGenerationErrors() ) ;
+		if ( result.getNumberOfGenerationErrors() > 0 ) {
+			int n = 0 ;
+			List<ErrorReport> errors = result.getErrors();
+			for ( ErrorReport error : errors ) {
+				n++;
+				System.out.println("ERROR #" + n);
+				System.out.println(" . error type    : " + error.getErrorType() );
+				System.out.println(" . error message : " + error.getMessage() );
+				Throwable ex = error.getException() ;
+				System.out.println(" . exception : " + ex.getMessage() );
+				Throwable cause = ex;
+				while ( ( cause = cause.getCause() ) != null ) {
+					System.out.println(" . cause : " + cause.getMessage() );
+				}
+			}
+		}
+		assertEquals(0, result.getNumberOfGenerationErrors() );
+		
+//		assertNotNull(model);
+//		assertEquals(3, model.getEntities().size() );
+//		assertNull(genericModelLoader.getErrorMessage());
+//		assertNotNull(e);
+//		assertEquals("Car", e.getClassName());
+//		assertEquals("Car", e.getDatabaseTable());
+
+	}
+
+}
---
+++ src/test/java/org/telosys/tools/api/ModelContentFromDSLModelTest.java
@@ -0,0 +1,116 @@
+package org.telosys.tools.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.telosys.tools.commons.cfg.TelosysToolsCfg;
+import org.telosys.tools.generic.model.Cardinality;
+import org.telosys.tools.generic.model.Entity;
+import org.telosys.tools.generic.model.Link;
+import org.telosys.tools.generic.model.Model;
+
+public class ModelContentFromDSLModelTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("Before test");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("After test");
+	}
+
+	private TelosysProject initProject() throws Exception {
+
+		String projectFolderFullPath = TestsEnv.createProjectFolder("myproject");
+		TelosysProject telosysProject = new TelosysProject(projectFolderFullPath);
+		
+		System.out.println("Init project...");
+		String s = telosysProject.initProject();
+		System.out.println(s);
+		
+//		TestsEnv.copyDbModelFile(projectFolderFullPath, "bookstore.dbrep");
+		TestsEnv.copyDslModelFiles(projectFolderFullPath, "employees");
+		TestsEnv.copyDslModelFiles(projectFolderFullPath, "cars-and-drivers");
+//		TestsEnv.copyDslModelFiles(projectFolderFullPath, "employees_invalid");
+//		TestsEnv.copyTemplatesFiles(projectFolderFullPath, "basic-templates-TT210");
+		
+		return telosysProject ;
+	}
+	
+	@Test
+	public void testModelDSL_Employees() throws Exception {
+		System.out.println("========== Loading .model ");
+		TelosysProject telosysProject = initProject() ;
+		System.out.println("getTelosysToolsCfg...");
+		TelosysToolsCfg telosysToolsCfg = telosysProject.getTelosysToolsCfg();
+		
+		GenericModelLoader genericModelLoader =  new GenericModelLoader(telosysToolsCfg);
+		Model model = genericModelLoader.loadModel("employees.model");
+		assertNotNull(model);
+		assertNull(genericModelLoader.getErrorMessage());
+		assertNull(genericModelLoader.getParsingErrors());
+		System.out.println("Model loaded : " + model.getEntities().size() + " entities");
+		
+		assertEquals(2, model.getEntities().size() );
+		
+		Entity e = model.getEntityByClassName("Employee");
+		assertNotNull(e);
+		assertEquals("Employee", e.getClassName());
+		assertEquals("Employee", e.getDatabaseTable());
+
+		List<Link> links = e.getLinks() ;
+		assertNotNull(links);
+		assertEquals(1, links.size() );
+		for ( Link link : links ) {
+			assertNotNull(link.getId());
+			assertNotNull(link.getFieldName());
+			assertNotNull(link.getFieldType());
+			assertEquals(Cardinality.MANY_TO_ONE, link.getCardinality() );
+		}
+	}
+
+	@Test
+	public void testModelDSL_CarsAndDrivers() throws Exception {
+		System.out.println("========== Loading .model ");
+		TelosysProject telosysProject = initProject() ;
+		System.out.println("getTelosysToolsCfg...");
+		TelosysToolsCfg telosysToolsCfg = telosysProject.getTelosysToolsCfg();
+		
+		GenericModelLoader genericModelLoader =  new GenericModelLoader(telosysToolsCfg);
+		Model model = genericModelLoader.loadModel("cars-and-drivers.model");
+		assertNotNull(model);
+		assertNull(genericModelLoader.getErrorMessage());
+		assertNull(genericModelLoader.getParsingErrors());
+		System.out.println("Model loaded : " + model.getEntities().size() + " entities");
+		
+		assertEquals(3, model.getEntities().size() );
+		
+		Entity e = model.getEntityByClassName("Car");
+		assertNotNull(e);
+		assertEquals("Car", e.getClassName());
+		assertEquals("Car", e.getDatabaseTable());
+
+		List<Link> links = e.getLinks() ;
+		assertNotNull(links);
+		assertEquals(2, links.size() );
+		for ( Link link : links ) {
+			System.out.println("Link Id : " + link.getId() );
+			assertNotNull(link.getId());
+			System.out.println("getFieldName() : " + link.getFieldName() );
+			assertNotNull(link.getFieldName());
+			System.out.println("getFieldType() : " + link.getFieldType() );
+			assertNotNull(link.getFieldType());
+			System.out.println("getCardinality() : " + link.getCardinality() );
+			assertEquals(Cardinality.MANY_TO_ONE, link.getCardinality() );
+		}
+	}
+
+}
