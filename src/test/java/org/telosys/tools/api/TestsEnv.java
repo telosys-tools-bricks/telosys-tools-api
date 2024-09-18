b5c4dab88bc6683c05e8a6143e0995cb93fa7650
l-gu <laurent@telosys.org>
2024-09-19 00:36:09 +0200

    Adaptations after FileUtil and ZipUtil changes

--- src/test/java/org/telosys/tools/api/TestsEnv.java
+++ src/test/java/org/telosys/tools/api/TestsEnv.java
@@ -7,40 +7,8 @@ import org.telosys.tools.commons.env.TelosysToolsEnv;
 
 public class TestsEnv {
 	
-//	public final static String REPO_FILENAME = "DERBY-Tests-Jan-2014-10.dbrep" ; 
-//	public final static String BUNDLE_NAME   = "basic-templates-TT210" ; 
-
-	public  final static String SRC_TEST_RESOURCES = "src/test/resources/" ;
-	
-	/***
-	private final static String TARGET_TESTS_TMP_DIR = "target/tests-tmp/" ;
-
-	private static File createVoidFolder(String folderName ) {
-		File folder = new File(TARGET_TESTS_TMP_DIR + folderName);
-		//--- Clean if exists
-		if ( folder.exists() ) {
-			if ( folder.isDirectory() ) {
-				DirUtil.deleteDirectory(folder);
-			}
-			else {
-				throw new RuntimeException("'"+ folder.getName() + "' is not a folder");
-			}
-		}
-		//--- Create
-		if ( folder.mkdirs() ) {
-			return folder ;
-		}
-		else {
-			throw new RuntimeException("Cannot create folder '"+ folder.getName() + "' ");
-		}
-	}
-
-	public static String createProjectFolder(String folderName ) {
-		File file = createVoidFolder( folderName ); 
-		return file.getAbsolutePath();
-	}
-***/
-	
+	public static final  String SRC_TEST_RESOURCES = "src/test/resources/" ;
+		
 	public static void copyDbModelFile(String projectFolderAbsolutePath, String dbrepFileName) throws Exception {
 		System.out.println("initializing 'DB model' in project from file '" + dbrepFileName +"'" );
 		
@@ -54,28 +22,14 @@ public class TestsEnv {
 		
 		System.out.println(" copy " + dbrepInputFile ); 
 		System.out.println("   to " + destDir ); 
-		FileUtil.copyToDirectory(dbrepInputFile, destDir, true);
+		FileUtil.copyFileInDirectory(dbrepInputFile, destDir, true);
 	}
 
 	public static void copyDslModelFiles(String projectFolderAbsolutePath, String dslModelName) throws Exception {
 		System.out.println("initializing 'DSL model' mane = '" + dslModelName +"'" );
-		//copyDslModelFile(projectFolderAbsolutePath, dslModelName+".model");
-		//copyDslModelFolder(projectFolderAbsolutePath, dslModelName+"_model");
 		copyDslModelFolder(projectFolderAbsolutePath, dslModelName);
 	}
-//	private static void copyDslModelFile(String projectFolderAbsolutePath, String dslModelFile) throws Exception {
-//		System.out.println("Copy 'DSL model' file '" + dslModelFile +"'" );
-//		String originalFileAbsolutePath = buildTestResourceAbsolutePath(FileUtil.buildFilePath("dsl-models", dslModelFile ));
-//		File dbrepInputFile = new File( originalFileAbsolutePath ) ;
-//		
-//		// destination folder
-//		String projectModelsFolder = FileUtil.buildFilePath(projectFolderAbsolutePath, TelosysToolsEnv.getInstance().getModelsFolder() );
-//		File destDir = new File(projectModelsFolder);
-//		
-//		System.out.println(" copy " + dbrepInputFile ); 
-//		System.out.println("   to " + destDir ); 
-//		FileUtil.copyToDirectory(dbrepInputFile, destDir, true);
-//	}
+
 	private static void copyDslModelFolder(String projectFolderAbsolutePath, String dslModelFolder) throws Exception {
 		System.out.println("Copy DSL model folder '" + dslModelFolder +"'" );
 		
@@ -89,7 +43,7 @@ public class TestsEnv {
 		
 		System.out.println(" copy " + sourceDir ); 
 		System.out.println("   to " + destDir ); 
-		FileUtil.copyFolder(sourceDir, destDir, true);
+		FileUtil.copyFolderToFolder(sourceDir, destDir, true);
 		System.out.println("Copy OK." ); 
 	}
 
@@ -106,7 +60,7 @@ public class TestsEnv {
 		
 		System.out.println(" copy " + sourceDir ); 
 		System.out.println("   to " + destDir ); 
-		FileUtil.copyFolder(sourceDir, destDir, true);
+		FileUtil.copyFolderToFolder(sourceDir, destDir, true);
 	}
 
 	/**
@@ -114,26 +68,9 @@ public class TestsEnv {
 	 * @param fileName
 	 * @return
 	 */
-	private final static String buildTestResourceAbsolutePath(String fileName) {
+	private static final String buildTestResourceAbsolutePath(String fileName) {
 		File file = new File(SRC_TEST_RESOURCES + fileName);
 		return file.getAbsolutePath();
 	}
-	
-
-//	public static void initBundle(TelosysProject telosysProject, String bundleName) throws TelosysToolsException, Exception {
-//		System.out.println("initializing bundle in project templates '" + bundleName +"'" );
-//		// original file is stored in "src/test/resources/"
-//		String originalFolderAbsolutePath = buildTestResourceAbsolutePath(FileUtil.buildFilePath("templates", bundleName ));
-//		File sourceFolder = new File( originalFolderAbsolutePath ) ;
-//		// destination folder
-//		TelosysToolsCfg telosysToolsCfg = telosysProject.loadTelosysToolsCfg();
-//		File destFolder = new File(telosysToolsCfg.getTemplatesFolderAbsolutePath(bundleName));
-//		
-//		
-//		//--- Copy 
-//		System.out.println(" from " + sourceFolder ); 
-//		System.out.println("   to " + destFolder ); 
-//		FileUtil.copyFolder(sourceFolder, destFolder, true);
-//	}
 
 }
